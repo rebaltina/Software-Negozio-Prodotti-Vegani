1a6c8b690f7802bcc5b1ad2a7986f716e8ba713a
rebaltina <rebaltina@users.noreply.github.com>
2024-06-18 10:12:41 +0200

    Applied corrections
    • L'esecuzione del codice non dovrebbe interrompersi dopo un solo inserimento di un comando in input. --> DONE. Ora il softare conitnua achiedere comandi finchè non viene digitato "chiudi"
    • Il software accetta prezzi e quantità negative --> DONE sia per "aggiungi" che per "vendita" 
    • All'inserimento di un prodotto già presente nel magazzino, il software non dovrebbe chiedere nuovamente prezzo di acquisto e prezzo di vendita  --> DONE
    • In fase di vendita, controllo che la quantità inserita sia effettivamente presente nel magazzino --> DONE, con qualche aggiunta. 1. se il prodotto non è oresente inmagazzino, il software chiede con un "print" di aggiungerelo (preferito a "assert" che interrompeva il programma) e poi apre un nuovo input di comando. 2. il software aggiorna la quantità rimanente in magazzino dopo la vendita
    • Il comando "elenca" dovrebbe dare in output la lista dei prodotti disponibili in magazzino, la loro quantità e il loro prezzo di acquisto --> DONE• Il comando "aiuto" non funziona correttamente 

--- actions.py
+++ actions.py
@@ -1,100 +1,181 @@
 def choose_an_input():
-    '''
-    helping user to choose a valid input
-    '''
+
+  action = input(f"Inserisci un comando ")
+
+  
+  help_menu = {"aggiungi": "aggiungi un prodotto al magazzino",
+  "elenca": "elenca i prodotto in magazzino",
+  "vendita": "registra una vendita effettuata",
+  "profitti": "mostra i profitti totali",
+  "aiuto" : "I comandi disponibili sono i seguenti: aggiungi: aggiungi un prodotto al magazzino elenca: elenca i prodotto in magazzino vendita: registra una vendita effettuata profitti: mostra i profitti totali aiuto: mostra i possibili comandi chiudi: esci dal programma",
+   "chiudi": "Esci dal programma"}
+
+  while action != "chiudi":
+
+    if action == "aggiungi":
+      add_new_product("inventory.json")
+    elif action == "elenca":
+      list_products("inventory.json")
+    elif action == "vendita":
+      add_new_sale("inventory.json", "vendite.json")
+    elif action == "profitti":
+      calculate_profits("vendite.json", "inventory.json")
+    elif (action not in help_menu.keys()) | (action == "aiuto"):
+      print(f'Comando non valido. I comandi disponibili sono i seguenti: {help_menu.items()}')
+
     action = input(f"Inserisci un comando ")
 
-    help_menu = {"aggiungi": "aggiungi un prodotto al magazzino", 
-    "elenca": "elenca i prodotto in magazzino",
-    "vendita": "registra una vendita effettuata",
-    "profitti": "mostra i profitti totali",
-    "aiuto" : "mostra i possibili comandi",
-    "chiudi": "esci dal programma" }
+  if action == "chiudi":
 
-    if action not in help_menu.keys():
-      print(f'Comando non valido. I comandi disponibili sono i seguenti: {help_menu.items()}')
-    else:
+      print("Bye bye") #only remaining command is "chiudi"
 
-      print(f"Ora {help_menu[action]}")
-
-      return action
+      return
 
 def add_new_product(filename):
     '''
     :param filename: inventory filename
     '''
-    inventory = {}
+    inventory = read_file(filename)
     product = input("Nome del prodotto: ")
 
     quantity = 0
-    while quantity == 0:
+
+    while quantity <= 0:
       try:
         quantity = int(input("quantità: "))
-      except:
-        print("il numero inserito non è un numero!")
-
+        if quantity < 0:
+          raise ValueError("il numero inserito non è un numero intero positivo!")
+      except ValueError:
+        print("l'input inserito non è un numero intero!")     
 
     if product in inventory.keys():
+      
+      print(f"prodotto {product} già presente in inventory: aggiornamento quantità di {product} in corso..")
+
       inventory[product]["quantità"]+=quantity
+      
+      
     else:
+
       cost = 0
       price = 0
-      while cost == 0:
+
+      while cost <= 0:
         try:
           cost = float(input("Prezzo di acquisto: "))
-        except:
-          print("l'input inserito non è un numero!")
-      while price == 0:
+          if cost < 0:
+            raise ValueError("il numero inserito non è un numero positivo!")
+        except ValueError:
+          print("il numero inserito non è un numero intero!")
+
+      while price <= 0:
         try:
           price = float(input("Prezzo di vendita: "))
-        except:
-          print("l'input inserito non è un numero!")
+          if price < 0:
+            raise ValueError("il numero inserito non è un numero positivo!")
+        except ValueError:
+          print("il numero inserito non è un numero valido! Ex. prezzo valido: 3.5")
 
       inventory[product] = {"quantità": quantity, "prezzo di vendita": price , "prezzo di acquisto" : cost}
 
-      insert_data_in_database(filename, inventory)
+    insert_data_in_database(filename, inventory)
+
+def list_products(filename):
+
+    inventory = read_file (filename)
+
+    print(f"i prodotti disponibili, in magazzino sono i seguenti:")
+    for key in inventory.keys():
+      
+      print(f'{inventory[key]["quantità"]} di {key} al prezzo di  {inventory[key]["prezzo di acquisto"]} euro')
 
 
-def add_new_sale(inventory_filename, sales_filename, date): 
+def add_new_sale(inventory_filename, sales_filename):
 
     '''
     :param inventory_filename: overwritten or created (if it does not exist) file name for storing sales
     :param date: date of the sales, previously obtained
     '''
+    today = datetime.datetime.now()
+    date = today.strftime("%m/%d/%Y, %H:%M:%S")
 
     sales = {}
 
     inventory = read_file (inventory_filename)
 
     product = input("Nome del prodotto: ")
-    assert(product in inventory.keys()), "il prodotto inserito non si trova in magazzino: per aggiungere un nuovo prodotto inserire il comando 'aggiungi'"
-      
-    quantity = int(input("quantità: "))
-    total_sale = inventory[product]["prezzo di vendita"] * quantity
-    sales[date] = {"products": [{}], "total_sales" : 0.0}    
-    sales[date]["products"][0][product] = {"quantità": quantity, "total_spent": total_sale}
-    sales[date]["total_sales"] += total_sale
+    #assert(product in inventory.keys()), "il prodotto inserito non si trova in magazzino: per aggiungere un nuovo prodotto inserire il comando 'aggiungi'"
+
+    if product not in inventory.keys():
+
+      print(f"{product} non si trova in magazzino: per aggiungere un nuovo prodotto inserire il comando 'aggiungi' e ripetere l'intera vendita")
+
+      choose_an_input()
+
+    quantity = 0
+
+    while quantity <= 0:
+      try:
+        quantity = int(input("quantità: "))
+        if quantity < 0:
+          raise ValueError("il numero inserito non è un numero intero positivo!")
+      except ValueError:
+        print("il numero inserito non è un numero intero positivo!")
+
+    while quantity > inventory[product]["quantità"]:
+
+      print(f'Attualmente la quantità di {product} disponibile in magazzino è di {inventory[product]["quantità"]}: \n inserisci una quantità inferiore')
+      quantity = int(input("quantità: "))
+
+    inventory[product]["quantità"] -= quantity
+
+    total_spent = inventory[product]["prezzo di vendita"] * quantity
+    sales[date] = {"products": [{}], "total_sales" : 0.0}
+    sales[date]["products"][0][product] = {"quantità": quantity, "total_spent": total_spent}
+    sales[date]["total_sales"] += total_spent
 
     new_sale = input("Aggiungere un altro prodotto ?(si/no): ")
-  
+
     if new_sale == "no":
 
-      insert_data_in_database("vendite.json", sales)
+      insert_data_in_database(sales_filename, sales)
+      insert_data_in_database(inventory_filename, inventory)
 
-      return sales
+      print("vendita registrata!")
+
+      return 
 
     elif new_sale == "si":
 
       product = input("Nome del prodotto: ")
-      assert(product in inventory.keys()), "il prodotto inserito non si trova in magazzino: per aggiungere un nuovo prodotto inserire il comando 'aggiungi'"
-      
+      #assert(product in inventory.keys()), "il prodotto inserito non si trova in magazzino: per aggiungere un nuovo prodotto inserire il comando 'aggiungi'"
+      if product not in inventory.keys():
+        print(f"{product} non si trova in magazzino: per aggiungere un nuovo prodotto inserire il comando 'aggiungi' e ripetere l'intera vendita")
+
+        choose_an_input()
+
+
       quantity = int(input("quantità: "))
-      sales[date]["products"][0][product] = {"quantità": quantity, "total_spent": total_sale}
-      sales[date]["total_sales"] += total_sale
+      while quantity <= 0:
+        try:
+          quantity = int(input("quantità: "))
+          if quantity < 0:
+            raise ValueError("il numero inserito non è un numero intero positivo!")
+        except ValueError:
+          print("il numero inserito non è un numero intero positivo!") 
 
-    insert_data_in_database("vendite.json", sales)
+      inventory[product]["quantità"] -= quantity
+      total_spent = inventory[product]["prezzo di vendita"] * quantity
+      sales[date]["products"][0][product] = {"quantità": quantity, "total_spent": total_spent}
+      sales[date]["total_sales"] += total_spent
 
-    return sales
+    
+    insert_data_in_database(sales_filename, sales)
+    insert_data_in_database(inventory_filename, inventory)
+
+    print("vendita registrata!")
+
+    return 
 
 def calculate_gross_income (sales):
     '''
@@ -112,7 +193,7 @@ def extract_sold_products(sold_products):
     :param sold_products: list of nested dictionaries where keys are product names and values are dictionary for quantities and expenses
     '''
     sold_quantity_product = {}
-    
+
     for index in range(len(sold_products)):
       sale = sold_products[index]
       for index_b in range(len(sale)):
@@ -150,6 +231,6 @@ def calculate_profits(sales_filename, inventory_filename):
 
 
     print(f"Il profitto lordo attuale è di {gross_income} euro")
-    print(f"Il profitto netto attuale è di {gross_income-total_costs} euro")
+    print(f"Il profitto netto attuale è di {gross_income-total_costs}")
 
     return
