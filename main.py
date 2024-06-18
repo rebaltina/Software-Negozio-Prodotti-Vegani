71f3a9d4ba1390de607245f3d0d409fb6f0ca819
rebaltina <rebaltina@users.noreply.github.com>
2024-06-18 10:20:31 +0200

    Update main.py with imports
    • Il main.py inserito nel GitHub non funziona correttamente poichè non hai importato gli altri moduli actions.py e utils.py -->DONE

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
 import datetime
 import json
+from actions import *
+from utils import *
 
 def main():
-  action = choose_an_input()
 
+  choose_an_input()
 
-  if action == "aggiungi":
-
-    add_new_product("inventory.json")
-    
-
-  elif action == "elenca":
-
-    inventory = read_file ("inventory.json")
-    product_list = [key for key in inventory.keys()]
-
-    print(f"i prodotti disponibili in magazzino sono i seguenti:{product_list}")
-
-  elif action == "vendita":  
-
-    today = datetime.datetime.now()
-    date = today.strftime("%m/%d/%Y, %H:%M:%S")
-    sales = add_new_sale("inventory.json", "vendite.json", date)
-
-
-
-  elif action == "profitti":
-
-    calculate_profits("vendite.json", "inventory.json")
-
-  else:
-
-    print("bye bye") #only remaining command is "chiudi"
 
 if __name__ == '__main__':
     main()
